d553c80b47c2730448ef0ee0d91f75533a0ed644
Kamva-pro <kamvelihlefatman@gmail.com>
2024-07-09 15:05:24 +0200

    added dummy code for home screen

---
+++ frontend/screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      {/* Add home screen content here */}
+    </div>
+  );
+};
+
+export default Home;
